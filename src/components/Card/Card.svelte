5f85ab5da0b54e4718c4f31cac49a72a359837df
Faqih <e0052804@u.nus.edu>
2024-07-17 16:10:09 +0800

    Create card for projects on portfolio

---
+++ src/components/Card/Card.svelte
@@ -0,0 +1,41 @@
+<script>
+	export let imgUrl = '';
+	export let imgAlt = '';
+	export let title = '';
+	export let description = '';
+</script>
+
+<div class="card">
+	<img src={imgUrl} alt={imgAlt} />
+	<div class="details">
+		<h1 class="cardTitle">{title}</h1>
+		<p class="cardDes">{description}</p>
+	</div>
+</div>
+
+<style>
+	.card {
+		width: 30%;
+		padding: 10px;
+		border: 1px solid rgba(0, 0, 0, 0.1);
+		box-sizing: border-box;
+	}
+	img {
+		max-width: 100%;
+		margin-bottom: 10px;
+		object-fit: cover;
+	}
+	.details {
+		border-top: 1px solid rgba(0, 0, 0, 0.1);
+		padding: 20px 20px 10px 20px;
+	}
+	.cardTitle {
+		font-size: 18px;
+		margin: 0;
+	}
+	.cardDes {
+		font-size: 15px;
+		color: #767676;
+		margin: 10px 0;
+	}
+</style>
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,16 +1,60 @@
+<script>
+	import Card from '../../components/Card/Card.svelte';
+</script>
+
 <div class="container">
-    <h1>// Portfolio</h1>
+	<div class="containerWrapper">
+		<h1 class="pageTitle">// Portfolio</h1>
+		<div class="projects">
+			<Card imgUrl="self-photo.jpg" title="Faqih" description="I am me" />
+			<Card imgUrl="self-photo.jpg" title="Faqih" description="I am me" />
+			<Card imgUrl="self-photo.jpg" title="Faqih" description="I am me" />
+			<Card imgUrl="self-photo.jpg" title="Faqih" description="I am me" />
+			<Card imgUrl="self-photo.jpg" title="Faqih" description="I am me" />
+			<Card imgUrl="self-photo.jpg" title="Faqih" description="I am me" />
+			<Card imgUrl="self-photo.jpg" title="Faqih" description="I am me" />
+			<Card imgUrl="self-photo.jpg" title="Faqih" description="I am me" />
+		</div>
+	</div>
 </div>
 
 <style>
-    .container {
-        width: calc(100% - 350px);
-        box-sizing: border-box;
-        position: absolute;
-        top: 0;
-        right: 0;
-        padding: 0 120px;
-        min-height: 100vh;
-        background-color: #f9f9f9;
-    }
+	.container {
+		display: flex;
+		justify-content: center;
+		width: calc(100% - 350px);
+		box-sizing: border-box;
+		position: absolute;
+		top: 0;
+		right: 0;
+		padding: 0 40px;
+		min-height: 100vh;
+		background-color: #f9f9f9;
+	}
+
+	.containerWrapper {
+		max-width: 1240px;
+		padding: 80px 0;
+	}
+
+	.pageTitle {
+		font-size: 18px;
+		font-weight: 700;
+		letter-spacing: 5px;
+		text-transform: uppercase;
+		margin-bottom: 70px;
+	}
+
+	.projects {
+		display: flex;
+		gap: 25px;
+		flex-wrap: wrap;
+		box-sizing: border-box;
+	}
+
+	@media only screen and (max-width: 1600px) {
+		.containerWrapper {
+			max-width: 968px;
+		}
+	}
 </style>
