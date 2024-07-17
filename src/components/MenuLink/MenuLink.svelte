1593795058bc312473b20692da55d9278cdc8a36
Faqih <e0052804@u.nus.edu>
2024-07-17 16:13:16 +0800

    Add transition to menu link

--- src/components/MenuLink/MenuLink.svelte
+++ src/components/MenuLink/MenuLink.svelte
@@ -1,44 +1,51 @@
 <script>
-    import { hover } from "../Cursor/stores";
+	import { hover } from '../Cursor/stores';
 
-    export let isActive = false;
-    export let href = "";
-    export let text = "";
-    export let tablet = false;
+	export let isActive = false;
+	export let href = '';
+	export let text = '';
+	export let tablet = false;
 </script>
 
-<li class:isActive on:mouseenter={() => hover.set(true)} on:mouseleave={() => hover.set(false)}>
-    <a class:tablet {href}>{text}</a>
+<li class:isActive>
+	<a
+		class:tablet
+		on:mouseenter={() => hover.set(true)}
+		on:mouseleave={() => hover.set(false)}
+		{href}>{text}</a
+	>
 </li>
 
 <style>
-    li {
-        list-style: none;
-        margin: 0 0 10px;
-    }
-    
-    a {
-        color: #bbb;
-        text-decoration: none;
-        font-optical-sizing: auto;
-        font-weight: 500;
-        font-style: normal;
-        font-size: 13px;
-        line-height: 1.8;
-        text-transform: uppercase;
-    }
-    a.tablet {
-        color: #000;
-    }
+	li {
+		list-style: none;
+		margin: 0 0 10px;
+	}
 
-    li:hover a:not(.tablet),
-    li.isActive a:not(.tablet) {
-        color: #fff;
-        transition: color 0.2s cubic-bezier(0.61,0.29,0,0.89);
-    }
-    li:hover a.tablet,
-    li.isActive a.tablet {
-        color: #bbb;
-        transition: color 0.2s cubic-bezier(0.61,0.29,0,0.89);
-    }
+	a {
+		color: #bbb;
+		text-decoration: none;
+		font-optical-sizing: auto;
+		font-weight: 500;
+		font-style: normal;
+		font-size: 13px;
+		line-height: 1.8;
+		text-transform: uppercase;
+		transition: color 0.9s cubic-bezier(0.61, 0.29, 0, 0.89);
+	}
+	a.tablet {
+		color: #000;
+		transition: color 0.2s cubic-bezier(0.61, 0.29, 0, 0.89);
+	}
+
+	li:hover a:not(.tablet),
+	li.isActive a:not(.tablet) {
+		color: #fff;
+		transition: color 0.2s cubic-bezier(0.61, 0.29, 0, 0.89);
+	}
+	li:hover a.tablet,
+	li.isActive a.tablet {
+		color: #bbb;
+		transition: color 0.2s cubic-bezier(0.61, 0.29, 0, 0.89);
+	}
 </style>
