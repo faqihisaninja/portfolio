c571c876801b4d077f821d668c86098d2ba70393
Faqih <e0052804@u.nus.edu>
2024-07-17 17:29:46 +0800

    Create contact page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,16 +1,78 @@
+<script>
+	import Button from '../../components/Button/Button.svelte';
+
+	let name = '';
+	let email = '';
+	let phone = '';
+	let subject = '';
+	let message = '';
+</script>
+
 <div class="container">
-    <h1>Contact</h1>
+	<h1 class="pageTitle">// Contact</h1>
+	<form class="form">
+		<div class="row">
+			<input bind:value={name} type="text" placeholder="Name" />
+			<input bind:value={email} type="text" placeholder="Email" />
+		</div>
+		<div class="row">
+			<input bind:value={phone} type="text" placeholder="Phone" />
+			<input bind:value={subject} type="text" placeholder="Subject" />
+		</div>
+		<div class="row">
+			<textarea class="messageInput" bind:value={message} placeholder="Message" />
+		</div>
+		<Button title="Send message" />
+	</form>
 </div>
 
 <style>
-    .container {
-        width: calc(100% - 350px);
-        box-sizing: border-box;
-        position: absolute;
-        top: 0;
-        right: 0;
-        padding: 0 120px;
-        min-height: 100vh;
-        background-color: #f9f9f9;
-    }
+	.container {
+		width: calc(100% - 350px);
+		box-sizing: border-box;
+		position: absolute;
+		top: 0;
+		right: 0;
+		padding: 80px 120px;
+		min-height: 100vh;
+		background-color: #f9f9f9;
+	}
+
+	.pageTitle {
+		font-size: 18px;
+		font-weight: 700;
+		letter-spacing: 5px;
+		text-transform: uppercase;
+		margin-bottom: 70px;
+	}
+
+	.form {
+		display: flex;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		gap: 15px;
+	}
+	.form input,
+	.form textarea {
+		box-sizing: border-box;
+		padding: 10px 20px;
+		border: 1px solid rgba(0, 0, 0, 0.1);
+		background-color: transparent;
+		flex: 1;
+	}
+	.form .row {
+		display: flex;
+		width: 100%;
+		gap: 15px;
+	}
+	.messageInput {
+		height: 150px;
+		resize: none;
+	}
+
+	@media only screen and (max-width: 1600px) {
+		.container {
+			padding: 80px 40px;
+		}
+	}
 </style>
